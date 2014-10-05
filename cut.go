f8d1ec67f16ca2fea69478b07f62c6265c411f3c
Felix Geller <fgeller@gmail.com>
2014-10-05 22:03:24 +1300

    Playing with reader/writer.

--- cut.go
+++ cut.go
@@ -3,6 +3,8 @@ package main
 import "flag"
 import "os"
 import "fmt"
+import "bufio"
+import "io"
 
 const (
     fields_message string = "select only these fields"
@@ -17,6 +19,15 @@ func parseArguments(arguments []string) map[string]interface{} {
     return map[string]interface{}{
         "fields": *fields,
     }
+}
+
+func cut(input io.Reader, output io.Writer, arguments map[string]interface{}) {
+    bufInput := bufio.NewReader(input)
+
+    firstLine, _, _ := bufInput.ReadLine()
+    fmt.Println("read line:", string(firstLine))
+
+    io.WriteString(output, string(firstLine))
 }
 
 func main() {
--- cut_test.go
+++ cut_test.go
@@ -2,9 +2,22 @@ package main
 
 import "testing"
 import "github.com/stretchr/testify/assert"
+import "os"
+import "bytes"
 
 func TestArgumentParsing(t *testing.T) {
     expectedFields := "1,3,5"
     arguments := parseArguments([]string{"-f", expectedFields})
     assert.Equal(t, arguments["fields"], expectedFields)
+}
+
+func TestReadingFileToStdOut(t *testing.T) {
+    arguments := parseArguments([]string{"-f", "1"})
+    fileName := "sample.csv"
+    input, _ := os.Open(fileName)
+    defer input.Close()
+    output := bytes.NewBuffer(nil)
+    cut(input, output, arguments)
+
+    assert.Equal(t, "first name,last name,favorite pet", output.String())
 }
