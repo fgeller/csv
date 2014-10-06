02465b861d1455c82afd9dbfcb832f3a247ff64d
Felix Geller <fgeller@gmail.com>
2014-10-06 21:09:31 +1300

    Cutting fields.

--- cut.go
+++ cut.go
@@ -5,31 +5,67 @@ import "os"
 import "fmt"
 import "bufio"
 import "io"
+import "strings"
+import "strconv"
 
 const (
-    fields_message string = "select only these fields"
+    fields_message    string = "select only these fields"
+    delimiter_message string = "custom delimiter"
 )
 
 func parseArguments(arguments []string) map[string]interface{} {
     var flags = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
     var fields = flags.String("f", "default", fields_message)
+    var delimiter = flags.String("d", "default", delimiter_message)
 
     flags.Parse(arguments)
 
     return map[string]interface{}{
-        "fields": *fields,
+        "fields":    *fields,
+        "delimiter": *delimiter,
     }
 }
 
+func delimiter(arguments map[string]interface{}) string {
+    if givenDelimiter, ok := arguments["delimiter"]; ok {
+        return givenDelimiter.(string)
+    }
+
+    return "\t"
+}
+
+func selectedFields(arguments map[string]interface{}) []int64 {
+    splits := strings.Split(arguments["fields"].(string), ",")
+    numbers := make([]int64, len(splits))
+    for idx, stringNumber := range splits {
+        numbers[idx], _ = strconv.ParseInt(stringNumber, 10, 64)
+    }
+    return numbers
+}
+
 func cut(input io.Reader, output io.Writer, arguments map[string]interface{}) {
+
     reader := bufio.NewReader(input)
+    delimiter := delimiter(arguments)
+    selectedFields := selectedFields(arguments)
 
     for {
-        line, err := reader.ReadBytes('\n')
-        io.WriteString(output, string(line))
+        line, err := reader.ReadString('\n')
+        fields := strings.Split(line, delimiter)
+        collectedFields := make([]string, len(selectedFields))
+
+        for index, fieldIndex := range selectedFields {
+            collectedFields[index] = fields[fieldIndex-1]
+        }
+
+        newLine := fmt.Sprintln(strings.TrimSuffix(strings.Join(collectedFields, delimiter), "\n"))
+        io.WriteString(output, newLine)
+
         if err == io.EOF {
             break
         }
+
         if err != nil {
             fmt.Println("Encountered error while reading:", err)
             break
--- cut_test.go
+++ cut_test.go
@@ -4,7 +4,6 @@ import "testing"
 import "github.com/stretchr/testify/assert"
 import "os"
 import "bytes"
-import "io/ioutil"
 
 func TestArgumentParsing(t *testing.T) {
     expectedFields := "1,3,5"
@@ -12,10 +11,73 @@ func TestArgumentParsing(t *testing.T) {
     assert.Equal(t, expectedFields, arguments["fields"])
 }
 
-func TestReadingFileToStdOut(t *testing.T) {
+func TestCopyingEntireFile(t *testing.T) {
     arguments := parseArguments([]string{"-f", "1"})
     fileName := "sample.csv"
-    expected, _ := ioutil.ReadFile(fileName)
+    expected := `first name,last name,favorite pet
+hans,hansen,moose
+peter,petersen,monarch
+`
+    input, _ := os.Open(fileName)
+    defer input.Close()
+    output := bytes.NewBuffer(nil)
+    cut(input, output, arguments)
+
+    assert.Equal(t, string(expected), output.String())
+}
+
+func TestCuttingFirstColumn(t *testing.T) {
+    arguments := parseArguments([]string{"-f", "1", "-d", ","})
+    fileName := "sample.csv"
+    expected := `first name
+hans
+peter
+`
+    input, _ := os.Open(fileName)
+    defer input.Close()
+    output := bytes.NewBuffer(nil)
+    cut(input, output, arguments)
+
+    assert.Equal(t, string(expected), output.String())
+}
+
+func TestCuttingSecondColumn(t *testing.T) {
+    arguments := parseArguments([]string{"-f", "2", "-d", ","})
+    fileName := "sample.csv"
+    expected := `last name
+hansen
+petersen
+`
+    input, _ := os.Open(fileName)
+    defer input.Close()
+    output := bytes.NewBuffer(nil)
+    cut(input, output, arguments)
+
+    assert.Equal(t, string(expected), output.String())
+}
+
+func TestCuttingThirdColumn(t *testing.T) {
+    arguments := parseArguments([]string{"-f", "3", "-d", ","})
+    fileName := "sample.csv"
+    expected := `favorite pet
+moose
+monarch
+`
+    input, _ := os.Open(fileName)
+    defer input.Close()
+    output := bytes.NewBuffer(nil)
+    cut(input, output, arguments)
+
+    assert.Equal(t, string(expected), output.String())
+}
+
+func TestCuttingFirstAndThirdColumn(t *testing.T) {
+    arguments := parseArguments([]string{"-f", "1,3", "-d", ","})
+    fileName := "sample.csv"
+    expected := `first name,favorite pet
+hans,moose
+peter,monarch
+`
     input, _ := os.Open(fileName)
     defer input.Close()
     output := bytes.NewBuffer(nil)
