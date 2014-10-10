01183172ddc7448b121e758268e2cf7a06dc69b1
Felix Geller <fgeller@gmail.com>
2014-10-10 19:20:28 +1300

    Refactor tests to be from in memory string. Some indenting issue -_-

--- cut.go
+++ cut.go
@@ -8,210 +8,238 @@ import "strings"
 import "strconv"
 
 const (
-    fields_message    string = "select only these fields"
-    delimiter_message string = "custom delimiter"
+	fields_message    string = "select only these fields"
+	delimiter_message string = "custom delimiter"
 )
 
 // mhh... how to resolve naming conflict?
 type Range struct {
-    start int
-    end   int
+	start int
+	end   int
 }
 
 func (r Range) Contains(number int) bool {
-    switch {
-    case r.start == 0 && number <= r.end:
-        return true
-    case r.end == 0 && r.start <= number:
-        return true
-    case r.start <= number && number <= r.end:
-        return true
-    }
-    return false
+	switch {
+	case r.start == 0 && number <= r.end:
+		return true
+	case r.end == 0 && r.start <= number:
+		return true
+	case r.start <= number && number <= r.end:
+		return true
+	}
+	return false
 }
 
 func (r Range) String() string {
-    return fmt.Sprintf("Range(%v, %v)", r.start, r.end)
+	return fmt.Sprintf("Range(%v, %v)", r.start, r.end)
 }
 
 func NewRange(start int, end int) Range {
-    return Range{start: start, end: end}
+	return Range{start: start, end: end}
 }
 
 type parameters struct {
-    ranges    []Range
-    delimiter string
-    input     []*os.File
+	ranges    []Range
+	delimiter string
+	input     []*os.File
 }
 
 func openInput(fileNames []string) ([]*os.File, error) {
-    if 0 == len(fileNames) || fileNames[0] == "-" {
-        return []*os.File{os.Stdin}, nil
-    }
+	if 0 == len(fileNames) || fileNames[0] == "-" {
+		return []*os.File{os.Stdin}, nil
+	}
 
-    opened, err := openFiles(fileNames)
-    if err != nil {
-        return nil, err
-    }
+	opened, err := openFiles(fileNames)
+	if err != nil {
+		return nil, err
+	}
 
-    return opened, nil
+	return opened, nil
 }
 
 // TODO: -d" "
 func parseArguments(rawArguments []string) (*parameters, error) {
-    // default values
-    ranges := ""
-    delimiter := ","
-    fileNames := []string{}
+	// default values
+	ranges := ""
+	delimiter := ","
+	fileNames := []string{}
 
-    for index := 0; index < len(rawArguments); index += 1 {
-        argument := rawArguments[index]
-        switch {
-        case argument == "-f":
-            ranges = rawArguments[index+1]
-            index += 1
-        case strings.HasPrefix(argument, "-f"):
-            ranges = argument[2:]
-        case argument == "-d":
-            delimiter = rawArguments[index+1]
-            index += 1
-        case strings.HasPrefix(argument, "-d"):
-            delimiter = argument[2:]
-        case true:
-            fileNames = append(fileNames, argument)
-        }
-    }
+	for index := 0; index < len(rawArguments); index += 1 {
+		argument := rawArguments[index]
+		switch {
+		case argument == "-f":
+			ranges = rawArguments[index+1]
+			index += 1
+		case strings.HasPrefix(argument, "-f"):
+			ranges = argument[2:]
+		case argument == "-d":
+			delimiter = rawArguments[index+1]
+			index += 1
+		case strings.HasPrefix(argument, "-d"):
+			delimiter = argument[2:]
+		case true:
+			fileNames = append(fileNames, argument)
+		}
+	}
 
-    input, err := openInput(fileNames)
-    if err != nil {
-        return nil, err
-    }
+	input, err := openInput(fileNames)
+	if err != nil {
+		return nil, err
+	}
 
-    return &parameters{
-        ranges:    parseRanges(ranges),
-        delimiter: delimiter,
-        input:     input,
-    }, nil
+	return &parameters{
+		ranges:    parseRanges(ranges),
+		delimiter: delimiter,
+		input:     input,
+	}, nil
 }
 
 func openFiles(fileNames []string) ([]*os.File, error) {
-    files := make([]*os.File, len(fileNames))
+	files := make([]*os.File, len(fileNames))
 
-    for index, fileName := range fileNames {
-        file, err := os.Open(fileName)
-        if err != nil {
-            return nil, err
-        }
+	for index, fileName := range fileNames {
+		file, err := os.Open(fileName)
+		if err != nil {
+			return nil, err
+		}
 
-        files[index] = file
-    }
+		files[index] = file
+	}
 
-    return files, nil
+	return files, nil
 }
 
 func parseInt(raw string) int {
-    number, _ := strconv.ParseInt(raw, 10, 32)
-    return int(number)
+	number, _ := strconv.ParseInt(raw, 10, 32)
+	return int(number)
 }
 
 func parseRange(raw string) Range {
-    splitPosition := strings.Index(raw, "-")
+	splitPosition := strings.Index(raw, "-")
 
-    if splitPosition == -1 {
-        number := parseInt(raw)
-        return NewRange(number, number)
-    }
+	if splitPosition == -1 {
+		number := parseInt(raw)
+		return NewRange(number, number)
+	}
 
-    lower := raw[:splitPosition]
-    upper := raw[splitPosition+1:]
+	lower := raw[:splitPosition]
+	upper := raw[splitPosition+1:]
 
-    return NewRange(parseInt(lower), parseInt(upper))
+	return NewRange(parseInt(lower), parseInt(upper))
 }
 
 func parseRanges(rawRanges string) []Range {
-    if 0 == len(rawRanges) {
-        return []Range{}
-    }
+	if 0 == len(rawRanges) {
+		return []Range{}
+	}
 
-    ranges := make([]Range, 0)
-    for _, raw := range strings.Split(rawRanges, ",") {
-        ranges = append(ranges, parseRange(raw))
-    }
+	ranges := make([]Range, 0)
+	for _, raw := range strings.Split(rawRanges, ",") {
+		ranges = append(ranges, parseRange(raw))
+	}
 
-    return ranges
+	return ranges
 }
 
 // TODO: can we do this lazily?
 func selectedFields(ranges []Range, total int) []int {
-    selected := make([]int, 0)
+	selected := make([]int, 0)
 outer:
-    for field := 1; field <= total; field += 1 {
-        for _, aRange := range ranges {
-            if aRange.Contains(field) {
-                selected = append(selected, field)
-                continue outer
-            }
-        }
-    }
+	for field := 1; field <= total; field += 1 {
+		for _, aRange := range ranges {
+			if aRange.Contains(field) {
+				selected = append(selected, field)
+				continue outer
+			}
+		}
+	}
 
-    return selected
+	return selected
 }
 
 func collectFields(fields []string, selected []int) []string {
-    if 0 == len(selected) {
-        return fields
-    }
+	if 0 == len(selected) {
+		return fields
+	}
 
-    collectedFields := make([]string, len(selected))
-    for index, selectedField := range selected {
-        collectedFields[index] = fields[selectedField-1]
-    }
+	collectedFields := make([]string, len(selected))
+	for index, selectedField := range selected {
+		collectedFields[index] = fields[selectedField-1]
+	}
 
-    return collectedFields
+	return collectedFields
 }
 
 // TODO take param with modes
 func cutFile(input io.Reader, output io.Writer, delimiter string, ranges []Range) {
-    reader := bufio.NewReader(input)
+	reader := bufio.NewReader(input)
 
-    for {
-        line, err := reader.ReadString('\n')
-        fields := strings.Split(line, delimiter)
+	for {
+		line, err := reader.ReadString('\n')
+		fields := strings.Split(line, delimiter)
 
-        selected := selectedFields(ranges, len(fields))
-        collectedFields := collectFields(fields, selected)
+		selected := selectedFields(ranges, len(fields))
+		collectedFields := collectFields(fields, selected)
 
-        newLine := fmt.Sprintln(strings.TrimSuffix(strings.Join(collectedFields, delimiter), "\n"))
-        _, writeErr := io.WriteString(output, newLine)
+		newLine := fmt.Sprintln(strings.TrimSuffix(strings.Join(collectedFields, delimiter), "\n"))
+		_, writeErr := io.WriteString(output, newLine)
 
-        if err == io.EOF {
-            break
-        }
+		if err == io.EOF {
+			break
+		}
 
-        if err != nil {
-            fmt.Println("Encountered error while reading:", err)
-            break
-        }
+		if err != nil {
+			fmt.Println("Encountered error while reading:", err)
+			break
+		}
 
-        if writeErr != nil {
-            fmt.Println("Encountered error while writing:", writeErr)
-            break
-        }
-    }
+		if writeErr != nil {
+			fmt.Println("Encountered error while writing:", writeErr)
+			break
+		}
+	}
 }
 
 func cut(arguments []string, output io.Writer) {
-    parameters, err := parseArguments(arguments)
-    if err != nil {
-        fmt.Println("Invalid arguments:", err)
-        return
-    }
+	parameters, err := parseArguments(arguments)
+	if err != nil {
+		fmt.Println("Invalid arguments:", err)
+		return
+	}
 
-    for _, file := range parameters.input {
-        cutFile(file, output, parameters.delimiter, parameters.ranges)
-    }
+	for _, file := range parameters.input {
+		cutFile(file, output, parameters.delimiter, parameters.ranges)
+	}
 }
 
+// -b, --bytes=LIST
+//        select only these bytes
+
+// -c, --characters=LIST
+//        select only these characters
+
+// -d, --delimiter=DELIM
+//        use DELIM instead of TAB for field delimiter
+
+// -f, --fields=LIST
+//        select only these fields;  also print any line that contains no delimiter character, unless the -s option is specified
+
+// -n     (ignored)
+
+// --complement
+//        complement the set of selected bytes, characters or fields
+
+// -s, --only-delimited
+//        do not print lines not containing delimiters
+
+// --output-delimiter=STRING
+//        use STRING as the output delimiter the default is to use the input delimiter
+
+// --help display this help and exit
+
+// --version
+//        output version information and exit
+
 func main() {
-    cut(os.Args[1:], os.Stdout)
+	cut(os.Args[1:], os.Stdout)
 }
--- cut_test.go
+++ cut_test.go
@@ -5,196 +5,211 @@ import "os"
 import "bytes"
 import "fmt"
 import "reflect"
+import "strings"
 
 func assert(t *testing.T, expected interface{}, actual interface{}) {
-    if !reflect.DeepEqual(expected, actual) {
-        t.Error(
-            "Expected", fmt.Sprintf("[%v]", expected),
-            "\n",
-            "Actual", fmt.Sprintf("[%v]", actual))
-    }
+	if !reflect.DeepEqual(expected, actual) {
+		t.Error(
+			"Expected", fmt.Sprintf("[%v]", expected),
+			"\n",
+			"Actual", fmt.Sprintf("[%v]", actual))
+	}
 }
 
 func TestFieldsArgumentParsing(t *testing.T) {
-    arguments, _ := parseArguments([]string{fmt.Sprint("-f", "1,3,5")})
-    assert(t, []Range{NewRange(1, 1), NewRange(3, 3), NewRange(5, 5)}, arguments.ranges)
+	arguments, _ := parseArguments([]string{fmt.Sprint("-f", "1,3,5")})
+	assert(t, []Range{NewRange(1, 1), NewRange(3, 3), NewRange(5, 5)}, arguments.ranges)
 
-    arguments, _ = parseArguments([]string{"-f", "1,3,5"})
-    assert(t, []Range{NewRange(1, 1), NewRange(3, 3), NewRange(5, 5)}, arguments.ranges)
+	arguments, _ = parseArguments([]string{"-f", "1,3,5"})
+	assert(t, []Range{NewRange(1, 1), NewRange(3, 3), NewRange(5, 5)}, arguments.ranges)
 
-    arguments, _ = parseArguments([]string{})
-    assert(t, []Range{}, arguments.ranges)
+	arguments, _ = parseArguments([]string{})
+	assert(t, []Range{}, arguments.ranges)
 
-    arguments, _ = parseArguments([]string{"-f", "1-3"})
-    assert(t, []Range{NewRange(1, 3)}, arguments.ranges)
+	arguments, _ = parseArguments([]string{"-f", "1-3"})
+	assert(t, []Range{NewRange(1, 3)}, arguments.ranges)
 
-    arguments, _ = parseArguments([]string{"-f", "1-"})
-    assert(t, []Range{NewRange(1, 0)}, arguments.ranges)
+	arguments, _ = parseArguments([]string{"-f", "1-"})
+	assert(t, []Range{NewRange(1, 0)}, arguments.ranges)
 
-    arguments, _ = parseArguments([]string{"-f", "1"})
-    assert(t, []Range{NewRange(1, 1)}, arguments.ranges)
+	arguments, _ = parseArguments([]string{"-f", "1"})
+	assert(t, []Range{NewRange(1, 1)}, arguments.ranges)
 
-    arguments, _ = parseArguments([]string{"-f", "-23"})
-    assert(t, []Range{NewRange(0, 23)}, arguments.ranges)
+	arguments, _ = parseArguments([]string{"-f", "-23"})
+	assert(t, []Range{NewRange(0, 23)}, arguments.ranges)
 
-    arguments, _ = parseArguments([]string{"-f", "1-3,5"})
-    assert(t, []Range{NewRange(1, 3), NewRange(5, 5)}, arguments.ranges)
+	arguments, _ = parseArguments([]string{"-f", "1-3,5"})
+	assert(t, []Range{NewRange(1, 3), NewRange(5, 5)}, arguments.ranges)
 
-    arguments, _ = parseArguments([]string{"-f", "1-3,-5,23,42-"})
-    assert(t, []Range{NewRange(1, 3), NewRange(0, 5), NewRange(23, 23), NewRange(42, 0)}, arguments.ranges)
+	arguments, _ = parseArguments([]string{"-f", "1-3,-5,23,42-"})
+	assert(t, []Range{NewRange(1, 3), NewRange(0, 5), NewRange(23, 23), NewRange(42, 0)}, arguments.ranges)
 }
 
 func TestDelimiterArgumentParsing(t *testing.T) {
-    arguments, _ := parseArguments([]string{"-d", ","})
-    assert(t, ",", arguments.delimiter)
+	arguments, _ := parseArguments([]string{"-d", ","})
+	assert(t, ",", arguments.delimiter)
 
-    arguments, _ = parseArguments([]string{"-d,"})
-    assert(t, ",", arguments.delimiter)
+	arguments, _ = parseArguments([]string{"-d,"})
+	assert(t, ",", arguments.delimiter)
 
-    arguments, _ = parseArguments([]string{})
-    assert(t, ",", arguments.delimiter)
+	arguments, _ = parseArguments([]string{})
+	assert(t, ",", arguments.delimiter)
 }
 
 func TestFileNameArgumentParsing(t *testing.T) {
-    arguments, _ := parseArguments([]string{"sample.csv"})
-    assert(t, "sample.csv", arguments.input[0].Name())
+	arguments, _ := parseArguments([]string{"sample.csv"})
+	assert(t, "sample.csv", arguments.input[0].Name())
 
-    arguments, _ = parseArguments([]string{})
-    assert(t, []*os.File{os.Stdin}, arguments.input)
+	arguments, _ = parseArguments([]string{})
+	assert(t, []*os.File{os.Stdin}, arguments.input)
 
-    arguments, _ = parseArguments([]string{"-"})
-    assert(t, []*os.File{os.Stdin}, arguments.input)
+	arguments, _ = parseArguments([]string{"-"})
+	assert(t, []*os.File{os.Stdin}, arguments.input)
 }
 
+var fullFile = `first name,last name,favorite pet
+hans,hansen,moose
+peter,petersen,monarch`
+
 var cutTests = []struct {
-    ranges    []Range
-    delimiter string
-    expected  string
+	ranges    []Range
+	delimiter string
+	input     string
+	expected  string
 }{
-    { // full file when no fields
-        ranges:    []Range{},
-        delimiter: ",",
-        expected: `first name,last name,favorite pet
+	{ // full file when no fields
+		ranges:    []Range{},
+		delimiter: ",",
+		input:     fullFile,
+		expected: `first name,last name,favorite pet
 hans,hansen,moose
 peter,petersen,monarch
 `,
-    },
-    { // full file when no delimiter
-        ranges:    []Range{NewRange(1, 1)},
-        delimiter: `\t`,
-        expected: `first name,last name,favorite pet
+	},
+	{ // full file when no delimiter
+		ranges:    []Range{NewRange(1, 1)},
+		delimiter: `\t`,
+		input:     fullFile,
+		expected: `first name,last name,favorite pet
 hans,hansen,moose
 peter,petersen,monarch
 `,
-    },
-    { // cutting first column
-        ranges:    []Range{NewRange(1, 1)},
-        delimiter: ",",
-        expected: `first name
+	},
+	{ // cutting first column
+		ranges:    []Range{NewRange(1, 1)},
+		delimiter: ",",
+		input:     fullFile,
+		expected: `first name
 hans
 peter
 `,
-    },
-    { // cutting second column
-        ranges:    []Range{NewRange(2, 2)},
-        delimiter: ",",
-        expected: `last name
+	},
+	{ // cutting second column
+		ranges:    []Range{NewRange(2, 2)},
+		delimiter: ",",
+		input:     fullFile,
+		expected: `last name
 hansen
 petersen
 `,
-    },
-    { // cutting third column
-        ranges:    []Range{NewRange(3, 3)},
-        delimiter: ",",
-        expected: `favorite pet
+	},
+	{ // cutting third column
+		ranges:    []Range{NewRange(3, 3)},
+		delimiter: ",",
+		input:     fullFile,
+		expected: `favorite pet
 moose
 monarch
 `,
-    },
-    { // cutting first and third column
-        ranges:    []Range{NewRange(1, 1), NewRange(3, 3)},
-        delimiter: ",",
-        expected: `first name,favorite pet
+	},
+	{ // cutting first and third column
+		ranges:    []Range{NewRange(1, 1), NewRange(3, 3)},
+		delimiter: ",",
+		input:     fullFile,
+		expected: `first name,favorite pet
 hans,moose
 peter,monarch
 `,
-    },
-    { // cutting first and second column via range
-        ranges:    []Range{NewRange(1, 2)},
-        delimiter: ",",
-        expected: `first name,last name
+	},
+	{ // cutting first and second column via range
+		ranges:    []Range{NewRange(1, 2)},
+		delimiter: ",",
+		input:     fullFile,
+		expected: `first name,last name
 hans,hansen
 peter,petersen
 `,
-    },
-    { // cutting all via a range
-        ranges:    []Range{NewRange(1, 0)},
-        delimiter: ",",
-        expected: `first name,last name,favorite pet
+	},
+	{ // cutting all via a range
+		ranges:    []Range{NewRange(1, 0)},
+		delimiter: ",",
+		input:     fullFile,
+		expected: `first name,last name,favorite pet
 hans,hansen,moose
 peter,petersen,monarch
 `,
-    },
-    { // cutting all via a range
-        ranges:    []Range{NewRange(0, 3)},
-        delimiter: ",",
-        expected: `first name,last name,favorite pet
+	},
+	{ // cutting all via a range
+		ranges:    []Range{NewRange(0, 3)},
+		delimiter: ",",
+		input:     fullFile,
+		expected: `first name,last name,favorite pet
 hans,hansen,moose
 peter,petersen,monarch
 `,
-    },
-    { // cutting all via a range
-        ranges:    []Range{NewRange(1, 3), NewRange(3, 3)},
-        delimiter: ",",
-        expected: `first name,last name,favorite pet
+	},
+	{ // cutting all via a range
+		ranges:    []Range{NewRange(1, 3), NewRange(3, 3)},
+		delimiter: ",",
+		input:     fullFile,
+		expected: `first name,last name,favorite pet
 hans,hansen,moose
 peter,petersen,monarch
 `,
-    },
+	},
 }
 
 func TestCutFile(t *testing.T) {
-    fileName := "sample.csv"
+	for _, data := range cutTests {
+		// input, _ := os.Open(fileName)
+		// defer input.Close()
+		input := strings.NewReader(data.input)
+		output := bytes.NewBuffer(nil)
 
-    for _, data := range cutTests {
-        input, _ := os.Open(fileName)
-        defer input.Close()
-        output := bytes.NewBuffer(nil)
+		cutFile(input, output, data.delimiter, data.ranges)
 
-        cutFile(input, output, data.delimiter, data.ranges)
-
-        assert(t, output.String(), data.expected)
-    }
+		assert(t, output.String(), data.expected)
+	}
 }
 
 func TestCut(t *testing.T) {
-    fileName := "sample.csv"
-    contents := `first name,last name,favorite pet
+	fileName := "sample.csv"
+	contents := `first name,last name,favorite pet
 hans,hansen,moose
 peter,petersen,monarch
 `
 
-    input, _ := os.Open(fileName)
-    defer input.Close()
-    output := bytes.NewBuffer(nil)
+	input, _ := os.Open(fileName)
+	defer input.Close()
+	output := bytes.NewBuffer(nil)
 
-    cut([]string{fileName}, output)
+	cut([]string{fileName}, output)
 
-    assert(t, string(contents), output.String())
+	assert(t, string(contents), output.String())
 }
 
 func TestCuttingMultipleFiles(t *testing.T) {
-    fileName := "sample.csv"
-    contents := `first name,last name,favorite pet
+	fileName := "sample.csv"
+	contents := `first name,last name,favorite pet
 hans,hansen,moose
 peter,petersen,monarch
 `
 
-    input, _ := os.Open(fileName)
-    defer input.Close()
-    output := bytes.NewBuffer(nil)
+	input, _ := os.Open(fileName)
+	defer input.Close()
+	output := bytes.NewBuffer(nil)
 
-    cut([]string{fileName, fileName}, output)
+	cut([]string{fileName, fileName}, output)
 
-    assert(t, fmt.Sprint(string(contents), string(contents)), output.String())
+	assert(t, fmt.Sprint(string(contents), string(contents)), output.String())
 }
