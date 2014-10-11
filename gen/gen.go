bfedb2284b3e592fa669548c0146336d2f687ef6
Felix Geller <fgeller@gmail.com>
2014-10-11 13:59:46 +1300

    Hook it up via command line arguments.

--- gen/gen.go
+++ gen/gen.go
@@ -3,12 +3,21 @@ package main
 import "math/rand"
 import "time"
 import "strings"
+import "os"
+import "io"
+import "fmt"
+import "strconv"
 
 type parameters struct {
 	fields        int
 	maxWordLength int
 	minWordLength int
 	lineCount     int
+}
+
+func (p *parameters) String() string {
+	return fmt.Sprintf("parameters(lines=%v, fields=%v, maxWordLength=%v, minWordLength=%v)",
+		p.lineCount, p.fields, p.maxWordLength, p.minWordLength)
 }
 
 func randomASCIIByte() byte {
@@ -43,4 +52,51 @@ func randomFile(parameters *parameters) string {
 	}
 
 	return strings.Join(randomLines, "\n")
+}
+
+func parseArguments(arguments []string) *parameters {
+
+	lineCount := 0
+	fieldCount := 0
+	minWordLength := 0
+	maxWordLength := 10
+
+	for _, argument := range arguments {
+		switch {
+		case strings.HasPrefix(argument, "-l"):
+			number, _ := strconv.ParseInt(argument[2:], 10, 32)
+			lineCount = int(number)
+
+		case strings.HasPrefix(argument, "-f"):
+			number, _ := strconv.ParseInt(argument[2:], 10, 32)
+			fieldCount = int(number)
+
+		case strings.HasPrefix(argument, "-cmax"):
+			number, _ := strconv.ParseInt(argument[5:], 10, 32)
+			maxWordLength = int(number)
+
+		case strings.HasPrefix(argument, "-cmin"):
+			number, _ := strconv.ParseInt(argument[5:], 10, 32)
+			minWordLength = int(number)
+		}
+	}
+
+	return &parameters{
+		lineCount:     lineCount,
+		fields:        fieldCount,
+		minWordLength: minWordLength,
+		maxWordLength: maxWordLength,
+	}
+}
+
+func gen(arguments []string, output io.Writer) {
+	parameters := parseArguments(arguments)
+	fmt.Println("Parsed parameters", parameters)
+	for lineCount := 1; lineCount <= parameters.lineCount; lineCount += 1 {
+		io.WriteString(output, fmt.Sprintln(randomLine(parameters)))
+	}
+}
+
+func main() {
+	gen(os.Args[1:], os.Stdout)
 }
--- gen/gen_test.go
+++ gen/gen_test.go
@@ -4,6 +4,7 @@ import "fmt"
 import "strings"
 import "testing"
 import "reflect"
+import "bytes"
 
 // TODO share
 func assert(t *testing.T, expected interface{}, actual interface{}) {
@@ -26,12 +27,48 @@ func TestRandomLineGenerator(t *testing.T) {
 func TestRandomFileGenerator(t *testing.T) {
 	for count := 0; count < 10; count += 1 {
 		file := randomFile(&parameters{fields: 3, minWordLength: 1, maxWordLength: 3, lineCount: 3})
-		fmt.Println("Got random file:", file)
+
 		lines := strings.Split(file, "\n")
 		assert(t, 3, len(lines))
 		for _, line := range lines {
 			assert(t, true, 2 <= strings.Count(line, ","))
 			assert(t, true, len(line) > 2 && len(line) < 12)
+		}
+	}
+}
+
+var genTestData = []struct {
+	arguments     []string
+	fields        int
+	minWordLength int
+	maxWordLength int
+	lineCount     int
+}{
+	{
+		arguments:     []string{"-l2", "-f3", "-cmax10", "-cmin1"},
+		fields:        3,
+		minWordLength: 1,
+		maxWordLength: 10,
+		lineCount:     2,
+	},
+}
+
+func TestGen(t *testing.T) {
+
+	for _, data := range genTestData {
+		output := bytes.NewBuffer(nil)
+		gen(data.arguments, output)
+
+		generated := output.String()
+		fmt.Printf("Got generated [%v]", generated)
+
+		lines := strings.Split(generated, "\n")
+		assert(t, data.lineCount+1, len(lines))
+
+		for _, line := range lines[:data.lineCount] {
+			assert(t, true, data.fields-1 <= strings.Count(line, ","))
+			assert(t, true, len(line) > (data.fields-1+data.fields*data.minWordLength))
+			assert(t, true, len(line) < (data.fields*data.maxWordLength+data.fields))
 		}
 	}
 }
